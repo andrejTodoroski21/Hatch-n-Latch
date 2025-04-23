109e09612efbec78111ffe4775e1a8aa6fc67bbf
Andrej Todoroski <106843061+andrejTodoroski21@users.noreply.github.com>
2025-04-23 12:04:06 -0400

    added images

--- Client/src/components/Homepage.jsx
+++ Client/src/components/Homepage.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
-
+import mono_baby from "Client/src/assets/mother_and_baby_monochrome.jpg"
 function HomePage() {
   return (
     <div className="max-w-4xl mx-auto p-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-8">
         <div>
           <img
-            src="https://via.placeholder.com/400"
-            alt="Service"
+            src={mono_baby}
+            alt="mother and baby"
             className="w-full rounded"
           />
           <p className="mt-2">
